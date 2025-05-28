840b83b0954da07e8feff9b0c39221b3ad7f4726
Dave Aholt <daveaholt@gmail.com>
2025-05-28 17:37:19 -0500

    player spawning, regrowing plants, and some y sort fixes

--- adventuregame_godot/Props/Plants/Plant.gd
+++ adventuregame_godot/Props/Plants/Plant.gd
@@ -1,7 +1,16 @@
 class_name Plant extends Node2D
+@onready var hit_box: HitBox = $HitBox
+const PLANT = preload("res://Props/Plants/Plant.tscn")
+@onready var collision_shape_2d: CollisionShape2D = $StaticBody2D/CollisionShape2D
+
 
 func _ready() -> void:
-	$HitBox.Damaged.connect(TakeDamage)
+	hit_box.Damaged.connect(TakeDamage)
 
-func TakeDamage(_damage: int)-> void:
-	queue_free()
+func TakeDamage(_hurt_box: HurtBox)-> void:
+	visible = false
+	collision_shape_2d.set_deferred("disabled", true)
+	await get_tree().create_timer(15.0).timeout
+	visible = true
+	collision_shape_2d.set_deferred("disabled", false)
+	
